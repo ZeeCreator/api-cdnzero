d3ac684c8662f58f3716711d600eb6cf0e13434e
Era Corp <zero@gmail.com>
2026-03-26 08:00:48 +0700

    VERCEL SUPPORT 3

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // Vercel Serverless Function Entry Point
-const app = require('./index');
+const app = require('../src/index');
 
 module.exports = async (req, res) => {
     // Handle request dengan express app
